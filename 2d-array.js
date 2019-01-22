bbea572888afd61b092a7c16f833f70bb5ec2cf5
Calvin Brownlee <calvinbrownlee@gmail.com>
2019-01-22 10:21:21 -0500

    feat: write nested for loop

--- 2d-array.js
+++ 2d-array.js
@@ -1,1 +1,18 @@
-function hourglassSum(arr) {}
+const array2D = [
+  [-9, -9, -9, 1, 1, 1],
+  [0, -9, 0, 4, 3, 2],
+  [-9, -9, -9, 1, 2, 3],
+  [0, 0, 8, 6, 6, 0],
+  [0, 0, 0, -2, 0, 0],
+  [0, 0, 1, 2, 4, 0]
+];
+
+function hourglassSum(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }
+}
+
+hourglassSum(array2D);
