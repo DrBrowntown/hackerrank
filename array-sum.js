60627bc5e0037f63d286b8d4a52193e79259226e
Calvin Brownlee <calvinbrownlee@gmail.com>
2019-01-29 10:45:12 -0500

    feat: Add description to array-sum

--- array-sum.js
+++ array-sum.js
@@ -1,3 +1,18 @@
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array
+// , , so return
+
+// .
+
+// Function Description
+
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+// simpleArraySum has the following parameter(s):
+
+//     ar: an array of integers
+
 function simpleArraySum(ar) {
   let sum = 0;
   for (var i = 0; i < ar.length; i++) {
