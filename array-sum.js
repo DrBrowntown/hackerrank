c32bdfe93f752ed855004842b2646daf1dbbe133
Calvin Brownlee <calvinbrownlee@gmail.com>
2019-01-29 10:44:12 -0500

    feat: Solve array-sum

--- array-sum.js
+++ array-sum.js
@@ -1,5 +1,9 @@
 function simpleArraySum(ar) {
-  /*
-   * Write your code here.
-   */
+  let sum = 0;
+  for (var i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+  return sum;
 }
+
+simpleArraySum([1, 2, 3, 4, 10, 11]);
